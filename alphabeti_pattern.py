905116971bfcdbc61bd1e0aa6b35fa63daa633d3
Bittu722000 <72187991+Bittu722000@users.noreply.github.com>
2020-10-01 12:35:41 +0530

    Add files via upload

---
+++ alphabeti_pattern.py
@@ -0,0 +1,68 @@
+# Outer loop
+for i in range(65,70):
+    # Inner loop
+    for j in range(i,64,-1):
+        print(chr(j),end="")
+    print() 
+
+# outer loop
+for i in range (65,70):
+    # inner loop
+    for j in range(65,i+1):
+        print(chr(j),end="")
+    print()
+
+    # outer loop
+for i in range (65,70):
+    # inner loop
+    for j in range(65,i+1):
+        print(chr(i),end="")
+    print()
+
+    # Outer loop
+for i in range(65,70):
+    k=i
+    # Inner loop
+    for j in range(65,i+1):
+        print(chr(k),end="")
+        k=k+1
+    print()
+
+    str= "APOORVA"  # string
+# Outer loop
+for i in range(0,7):
+    # inner loop
+    for j in range(0,i+1):
+        print(str[j],end="")
+    print()
+
+    # Outer loop
+for i in range(65,70):
+    # Inner loop
+    for j in range(i,64,-1):
+        print(chr(j),end="")
+    print()
+
+    m=6   
+# Outer loop
+for i in range(65,70):
+    m=m-1
+    # Inner loop 1 
+    for j in range(m,1,-1):
+        print(" ",end="")
+    # Inner loop 2
+    for k in range(65,i+1):
+        print(chr(k),end="")
+    # Inner loop 3
+    for n in range(65,i):
+        print(chr(n),end="")
+    print()
+
+    # Outer loop
+for i in range(65,70):
+    # Inner loop
+    for j in range(i,64,-1):
+        print(chr(j),end="")
+    print()
+
+    
